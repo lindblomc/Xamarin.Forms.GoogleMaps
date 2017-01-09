fae890b18ae0ae788e49f0b74ee32c656748fa36
Claes Lindblom <claes.lindblom@cgi.com>
2017-01-09 23:32:07 +0100

    Added OnCameraChangeEvent and removed zoom controls on android

--- Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps.iOS/MapRenderer.cs
+++ Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps.iOS/MapRenderer.cs
@@ -170,6 +170,8 @@ namespace Xamarin.Forms.GoogleMaps.iOS
             var maxLat = Math.Max(Math.Max(Math.Max(region.NearLeft.Latitude, region.NearRight.Latitude), region.FarLeft.Latitude), region.FarRight.Latitude);
             var maxLon = Math.Max(Math.Max(Math.Max(region.NearLeft.Longitude, region.NearRight.Longitude), region.FarLeft.Longitude), region.FarRight.Longitude);
             mapModel.VisibleRegion = new MapSpan(new Position((minLat + maxLat) / 2d, (minLon + maxLon) / 2d), maxLat - minLat, maxLon - minLon);
+
+            Map.SendOnCameraChange(mkMapViewChangeEventArgs.Position.Target.ToPosition(), mkMapViewChangeEventArgs.Position.Zoom);
         }
 
         void CoordinateTapped(object sender, GMSCoordEventArgs e)
--- Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps/Map.cs
+++ Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps/Map.cs
@@ -39,6 +39,7 @@ namespace Xamarin.Forms.GoogleMaps
 
         public event EventHandler<MapClickedEventArgs> MapClicked;
         public event EventHandler<MapLongClickedEventArgs> MapLongClicked;
+        public event EventHandler<CameraChangedEventArgs> CameraChanged;
 
         MapSpan _visibleRegion;
 
@@ -239,6 +240,11 @@ namespace Xamarin.Forms.GoogleMaps
         internal void SendMapLongClicked(Position point)
         {
             MapLongClicked?.Invoke(this, new MapLongClickedEventArgs(point));
+        }
+
+        internal void SendOnCameraChange(Position point, double zoom)
+        {
+            CameraChanged?.Invoke(this, new CameraChangedEventArgs(point, zoom));
         }
     }
 }
