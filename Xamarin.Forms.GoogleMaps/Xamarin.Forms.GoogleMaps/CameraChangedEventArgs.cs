8806252d23007b4893e643be1d31b40d2df09d32
Claes Lindblom <claes.lindblom@cgi.com>
2017-01-10 08:00:10 +0100

    Added missing file from merge

---
+++ Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps/CameraChangedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Xamarin.Forms.GoogleMaps
+{
+    public class CameraChangedEventArgs : EventArgs
+    {
+        public Position Point { get; }
+        public double Zoom { get; }
+        public CameraChangedEventArgs(Position pos, double zoom)
+        {
+            this.Point = pos;
+            this.Zoom = zoom;
+        }
+    }
+}
